f3622455742e2deba19d5f0a558c3281ba526cf6
8baller <tomrae030@hotmail.com>
2023-05-11 08:23:06 +0100

    [feat] updated autonomy kubernetes to be key agnostic

--- autonomy/deploy/generators/kubernetes/templates.py
+++ autonomy/deploy/generators/kubernetes/templates.py
@@ -273,9 +273,6 @@ spec:
         - name: agent-key
           secret:
             secretName: agent-validator-{validator_ix}-key
-            items:
-            - key: private_key
-              path: ethereum_private_key.txt
         - name: persistent-data
           persistentVolumeClaim:
             claimName: 'logs-pvc'
@@ -306,7 +303,7 @@ spec:
 AGENT_SECRET_TEMPLATE: str = """
 apiVersion: v1
 stringData:
-    private_key: '{private_key}'
+    {ledger}_private_key.txt: '{private_key}'
 kind: Secret
 metadata:
   annotations:
